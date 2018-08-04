10e176b6a4c9db9a5541b36ad48316f3c774b40c
CheshireSwift <github@swifteagle.co.uk>
2018-08-04 18:49:47 +0100

    Made vanilla multiview support variable streams

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 import ChannelSwitcher from '../components/ChannelSwitcher'
 
+function channels() {
+  if (typeof window !== 'undefined' && window.URLSearchParams) {
+    const params = new URLSearchParams(window.location.search)
+    const streams = params.get('streams')
+    return streams.split(',')
+  }
+
+  return []
+}
+
 const IndexPage = () => (
   <ChannelSwitcher
-    channels={[
-'TeamSp00ky',
-'bgcallisto',
-'vgbootcamp',
-'funkyp',
-'pandaxgaming',
-'vampirearcadia',
-'capcomfighters',
-'tekken',
-'netherrealm',
-    ]}
+    channels={channels()}
   />
 )
 
